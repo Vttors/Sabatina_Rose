df0c566ee13f96ece8b8b534f3abf55b3bcba55a
Vttors <logares83@gmail.com>
2022-09-20 16:07:13 -0300

    Questoes 1-7 qissi = 0.65

--- ControladorPD.py
+++ ControladorPD.py
@@ -52,7 +52,7 @@ def Questao(questao,zero,k,s):
     print("Sistema: " + str(SISTEMA))
     print("Questão " + str(questao) + ".1: Figura " + str(figure))
     local_das_raizes(SISTEMA,questao)
-    print("Para um ξ de 0.62 temos:  k = " + str(k) + " e s = " + str(s))
+    print("Para um ξ de 0.65 temos:  k = " + str(k) + " e s = " + str(s))
     print("Questão " + str(questao) + ".2: ")
     margem_de_ganho_e_fase(SISTEMA*k)
     print("Questão " + str(questao) + ".3: Gráfico " + str(figure))
@@ -64,9 +64,9 @@ def Questao(questao,zero,k,s):
 print("Questão 1: Figura " + str(figure))
 local_das_raizes(G,1)
 
-print("Para um ξ de 0.62 temos:  k = 43.88 e s = -1.817 + 2.3j")
+print("Para um ξ de 0.65 temos:  k = 39.84 e s = -1.861 + 2.175j")
 
-k = 43.88
+k = 39.84
 
 # Questão 2:
 print("Questão 2:")
@@ -77,13 +77,13 @@ print("Questão 3: Gráfico " + str(figure))
 resposta_ao_degrau(G*k,3)
 
 # Questão 4:
-Questao(4,3,41.9, -4.609 + 5.832j)
+Questao(4, 3, 37.07, -4.626 + 5.408j)
 # Questão 5:
-Questao(5, 15, 3.765, -2.02 +2.556j)
+Questao(5, 15, 3.381, -2.052 +2.399j)
 # Questão 6:
-Questao(6, 30, 1.644, -1.905 + 2.411j)
+Questao(6, 30, 1.485, -1.944 + 2.273j)
 # Questão 7:
-Questao(7, 40, 1.196, -1.881 + 2.381j)
+Questao(7, 40, 1.082, -1.921 + 2.247j)
 
 
 plt.show()
