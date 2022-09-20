701ede40ed959d46372843c0b18accd11c91830b
Vttors <logares83@gmail.com>
2022-09-20 15:48:39 -0300

    Questão 1 - 7 qissi = 0.62

--- ControladorPD.py
+++ ControladorPD.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 
 figure = 1
 
-def local_das_raizes(sis):
+def local_das_raizes(sis,questao):
     global figure
     plt.figure(figure)
     figure += 1
     clt.rlocus(sis)
-    plt.title("Local das Raízes Original")
+    plt.title("Local das Raízes - Questão " + str(questao))
     plt.grid(visible = False)
 
 def margem_de_ganho_e_fase(sis):
@@ -23,21 +23,16 @@ def margem_de_ganho_e_fase(sis):
     else:
         print("O sistema não é estável")
 
-def resposta_ao_degrau(sis):
+def resposta_ao_degrau(sis, questao):
     global figure
     sis_fechado = clt.feedback(sis)
     y, x = clt.step_response(sis_fechado)
 
     plt.figure(figure)
     figure +=1
-    plt.title("Resposta ao Degrau")
+    plt.title("Resposta ao Degrau - Questão " + str(questao))
     degrau = np.ones(len(x))
     plt.plot(y,x,y,degrau)
-
-
-
-
-
 
 # Criando a função de trasnferência
 
@@ -48,58 +43,47 @@ G3 = clt.tf([1] , [1, 7])
 G = G1*G2*G3
 print("Sistema: " + str(G))
 
+def Questao(questao,zero,k,s):
+    # Adicionando o zero
+    Z1 = clt.tf([1,zero],[1])
+    SISTEMA = G * Z1
+
+    print("Questão "+ str(questao) + ":")
+    print("Sistema: " + str(SISTEMA))
+    print("Questão " + str(questao) + ".1: Figura " + str(figure))
+    local_das_raizes(SISTEMA,questao)
+    print("Para um ξ de 0.62 temos:  k = " + str(k) + " e s = " + str(s))
+    print("Questão " + str(questao) + ".2: ")
+    margem_de_ganho_e_fase(SISTEMA*k)
+    print("Questão " + str(questao) + ".3: Gráfico " + str(figure))
+    resposta_ao_degrau(SISTEMA*k,questao)
+
+
 
 # Questão 1:
 print("Questão 1: Figura " + str(figure))
-local_das_raizes(G)
+local_das_raizes(G,1)
 
 print("Para um ξ de 0.62 temos:  k = 43.88 e s = -1.817 + 2.3j")
 
-q = 0.62
 k = 43.88
-s = -1.87 + 2.3j
 
 # Questão 2:
 print("Questão 2:")
 margem_de_ganho_e_fase(G*k)
-# MG = 10.027
-# MF = 107.635 
-# Sistema Estável
 
 # Questão 3:
 print("Questão 3: Gráfico " + str(figure))
-resposta_ao_degrau(G*k)
+resposta_ao_degrau(G*k,3)
 
 # Questão 4:
-
-G4 = clt.tf([1,3],[1])
-G_ZERO3 = G * G4
-print("Questão 4:")
-print("Sistema: " + str(G_ZERO3))
-
-# Questão 4.1
-
-print("Questão 4.1: Figura " + str(figure))
-
-local_das_raizes(G_ZERO3)
-
-print("Para um ξ de 0.62 temos:  k = 41.9 e s = -4.609 + 5.832j")
-
-q = 0.62
-k = 41.9
-s = -4.609 + 5.832j
-
-# Questão 4.2:
-
-print("Questão 4.2: ")
-margem_de_ganho_e_fase(G_ZERO3*k)
-
-# Questão 4.3:
-print("Questão 4.3: Gráfico " + str(figure))
-resposta_ao_degrau(G_ZERO3*k)
+Questao(4,3,41.9, -4.609 + 5.832j)
+# Questão 5:
+Questao(5, 15, 3.765, -2.02 +2.556j)
+# Questão 6:
+Questao(6, 30, 1.644, -1.905 + 2.411j)
+# Questão 7:
+Questao(7, 40, 1.196, -1.881 + 2.381j)
 
 
 plt.show()
-
-
-
